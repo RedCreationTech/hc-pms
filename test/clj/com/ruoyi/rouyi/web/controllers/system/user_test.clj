f0b18547584b94bf4952e4ee9d20b6b690a971db
kevin li <lizhoayu1@redcreation.cn>
2026-06-12 16:32:07 +0800

    test: improve coverage to 65.65%
    Updated user controller test
    
    Coverage: 64.74% -> 65.65%

--- test/clj/com/ruoyi/rouyi/web/controllers/system/user_test.clj
+++ test/clj/com/ruoyi/rouyi/web/controllers/system/user_test.clj
@@ -5,11 +5,30 @@
 
 (def mock-user-service
   {:query-fn (fn [q p] (case q
-                          :list-users {:rows [{:user_id 1 :user_name "test"}] :total 1}
-                          :find-user-by-id {:user_id 1 :user_name "test"}
+                          :list-users {:rows [{:user_id 1 :user_name "admin" :nick_name "管理员" :email "admin@test.com" :phonenumber "13800138000" :sex "0" :status "0" :dept_id 1}] :total 1}
+                          :find-user-by-id {:user_id 1 :user_name "admin" :nick_name "管理员" :email "admin@test.com" :phonenumber "13800138000" :sex "0" :status "0" :dept_id 1 :role_ids [1] :post_ids [1]}
                           :create-user! [{:user_id 2}]
                           :update-user! nil
                           :delete-user! nil
+                          :reset-pwd! nil
+                          :change-status! nil
+                          :get-user-roles [{:role_id 1 :role_name "管理员"}]
+                          :get-user-posts [{:post_id 1 :post_name "董事长"}]
+                          []))})
+
+(def mock-dept-service
+  {:query-fn (fn [q p] (case q
+                          :list-depts [{:dept_id 1 :dept_name "总公司"}]
+                          []))})
+
+(def mock-role-service
+  {:query-fn (fn [q p] (case q
+                          :list-roles [{:role_id 1 :role_name "管理员"}]
+                          []))})
+
+(def mock-post-service
+  {:query-fn (fn [q p] (case q
+                          :list-posts [{:post_id 1 :post_name "董事长"}]
                           []))})
 
 (deftest test-list-users
@@ -26,13 +45,13 @@
 
 (deftest test-create-user
   (testing "创建用户"
-    (let [request {:body-params {:user_name "test" :nick_name "test" :password "123456"}}
+    (let [request {:body-params {:user_name "test" :nick_name "test" :password "123456" :role_ids [1] :post_ids [1]}}
           response (user/create-user {:user-service mock-user-service} request)]
       (is (map? response)))))
 
 (deftest test-update-user
   (testing "更新用户"
-    (let [request {:path-params {:id "1"} :body-params {:nick_name "updated"}}
+    (let [request {:path-params {:id "1"} :body-params {:nick_name "updated" :role_ids [1] :post_ids [1]}}
           response (user/update-user {:user-service mock-user-service} request)]
       (is (map? response)))))
 
@@ -41,3 +60,21 @@
     (let [request {:path-params {:id "1"}}
           response (user/delete-user {:user-service mock-user-service} request)]
       (is (map? response)))))
+
+(deftest test-reset-pwd
+  (testing "重置密码"
+    (let [request {:path-params {:id "1"} :body-params {:password "newpassword"}}
+          response (user/reset-password {:user-service mock-user-service} request)]
+      (is (map? response)))))
+
+(deftest test-change-status
+  (testing "修改状态"
+    (let [request {:path-params {:id "1"} :body-params {:status "1"}}
+          response (user/change-status {:user-service mock-user-service} request)]
+      (is (map? response)))))
+
+
+
+
+
+
